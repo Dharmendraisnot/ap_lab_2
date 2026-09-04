753ce6e1dbfeb8541eaa73f96300a43722b0c9a2
Dharmendra Meena <rahul798712@gmail.com>
2026-09-04 20:50:47 +0530

    Added solution to Lab Exercise 3

---
+++ LE3/CurrentAccount.java
@@ -0,0 +1,18 @@
+package ap_lab_2.LE3;
+
+public class CurrentAccount extends BankAccount {
+    double overdraftLimit;
+
+    public CurrentAccount(String accountNumber, double balance, double overdraftLimit) {
+        super(accountNumber, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if(this.balance + this.overdraftLimit >= amount)
+            this.balance -= amount;
+        else
+            System.out.println("Overdraft limit exceeded!");
+    }
+}
